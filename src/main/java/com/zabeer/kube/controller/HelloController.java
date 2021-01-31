e30085f1939b7e3bbbc4c1fdd0e0d60d3e14005b
ZabeerS82 <mail2zabeer@gmail.com>
2021-01-31 18:15:54 +0800

    updates

--- src/main/java/com/zabeer/kube/controller/HelloController.java
+++ src/main/java/com/zabeer/kube/controller/HelloController.java
@@ -1,14 +1,16 @@
 package com.zabeer.kube.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
+        log.info("Hello endpoint invoked");
         String demoString = System.getenv().getOrDefault("DEMO_STRING", "Kubernetes");
         return String.format("Hello. This is a %s demo", demoString);
     }
